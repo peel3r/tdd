ff27a4a6d3fd85598d2ea0636069776b1762f872
peel3r <ismc2222@gmail.com>
2018-11-01 12:15:20 +0000

    refactor user stories

--- spec/features/candidate_stories_spec.rb
+++ spec/features/candidate_stories_spec.rb
@@ -1,45 +1,53 @@
 describe  "Candidate Stories" do
-# As a candidate
-# So candidate can write a cv to find a job
-# instruct candidate to write a cv
-it "so candidate can write a cv, tell candidate to write a cv" do
-  candidate = Candidate.new(5)
-     cv = Cv.new
-     expect {candidate.write(cv)}.not_to raise_error
-end
-# As a candidate
-# So candidate can send a cv to find a job
-# Instruct candidate to  send a cv
+  let(:candidate) {Candidate.new(5)}
+  let(:cv) {Cv.new}
 
-  it "so candidate can send a cv, tell candidate to send a cv" do
-    candidate = Candidate.new(5)
-    cv = Cv.new
-    allow(candidate).to receive(:applied?).and_return false
-    expect { candidate.send(cv)}.not_to raise_error
+  context 'when not applied' do
+    before do
+      allow(candidate).to receive(:applied?).and_return false
+    end
+    # As a candidate
+    # So candidate can write a cv to find a job
+    # instruct candidate to write a cv
+    it "so candidate can write a cv, tell candidate to write a cv" do
+      expect {candidate.write(cv)}.not_to raise_error
+    end
+
+    # As a candidate
+    # So candidate can send a cv to find a job
+    # Instruct candidate to  send a cv
+
+    it "so candidate can send a cv, tell candidate to send a cv" do
+      expect { candidate.send(cv)}.not_to raise_error
+    end
   end
 
-# As a candidate
-# So that candidate can send only 5 cv at a time
-# Candidate want to prevent sending cv when target reached
   context 'when target reached' do
-    it 'does not allow cv s to be sent' do
-      candidate = Candidate.new(5)
+    # As a candidate
+    # So that candidate can send only 5 cv at a time
+    # Candidate want to prevent sending cv when target reached
+
+    before do
       allow(candidate).to receive(:applied?).and_return false
-      cv = Cv.new
+    end
+
+    it 'does not allow cv s to be sent' do
       5.times do
         candidate.send(cv)
       end
       expect {candidate.send(cv)}.to raise_error("Cannot send your Cv. Sent target reached")
     end
   end
-# As a candidate
-# So candidate can avoid sending cv multiple time to the same employer
-# Candidate want to prevent sending cv s when already sent to the same employer
+
   context 'when applied' do
-    it 'does not allow cv to be sent' do
-      candidate = Candidate.new(5)
-      cv = Cv.new
+    # As a candidate
+    # So candidate can avoid sending cv multiple time to the same employer
+    # Candidate want to prevent sending cv s when already sent to the same employer
+
+    before do
       allow(candidate).to receive(:applied?).and_return true
+    end
+    it 'does not allow cv to be sent' do
       expect {candidate.send(cv)}.to raise_error 'Cannot send cv. You already applied to this employer '
     end
   end
